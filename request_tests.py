6d698b70a79bc8ad6d76a0bc375c0829b57798e9
bohdan_kulchytskyy <merseneries@gmail.com>
2019-12-04 17:01:43 +0200

    Add another test for requests library Add some_staff py file for test some staffs

--- request_tests.py
+++ request_tests.py
@@ -1,7 +1,7 @@
 import requests
+import datetime
 
 URL = "https://api.weather.com/v2/turbo/vt1observation?apiKey=d522aa97197fd864d36b418f39ebb323&format=json&geocode=49.23%2C28.47&language=uk-UA&units=m"
-PARAMS = {}
 response = requests.get(URL)
 data_response = response.json()
 weather_params = data_response["vt1observation"]
@@ -9,5 +9,14 @@ weather_params = data_response["vt1observation"]
 result_2 = ["Температува:" + str(weather_params["temperature"]), weather_params["phrase"],
             "За відчуттям:" + str(weather_params["feelsLike"])]
 
-for e in result_2:
-    print(e)
+# for e in result_2:
+#     print(e)
+
+URL = "https://api.cuex.com/v1/exchanges/usd?to_currency=uah&from_date="
+param = {}
+current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+response = requests.get(URL + current_date)
+data_json = response.json()
+selected_data = data_json["data"][0]["rate"]
+print("Money rate 1$ to UAH on:", current_date, "is", str(selected_data)[:5])
+
---
+++ some_staff.py
@@ -0,0 +1,8 @@
+
+for n, t in zip(range(1, 10), range(10, 1, -1)):
+    if n == 1:
+        print('  ' * t, n, '  ' * t, sep='')
+        previously = 5
+    else:
+        print('  ' * t, str(n) * previously, '  ' * t, sep='')
+        previously = previously + 4
