8de7c9f31edec4b495cb1da9ae7c61bc8d12f512
bohdan_kulchytskyy <merseneries@gmail.com>
2019-12-12 15:13:28 +0200

    Add modified processes.py

--- processes.py
+++ processes.py
@@ -1,32 +1,71 @@
+import csv
 import datetime
+import os
+import time
 
 import psutil
-from pandas import DataFrame
-
-from my_funcs import get_path
-
-CURRENT_DATE = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
-NAME = "pycharm64.exe"
-NAME_FILE = "test.csv"
 
 
-def csv_write(name_file, data):
-    cars = [['Honda Civic', 'Toyota Corolla', 'Ford Focus', 'Audi A4'],
-            [22000, 25000, 27000, 35000]]
-    df = DataFrame(cars)
-    df.to_csv(get_path(NAME_FILE), sep=";")
+class Monitor:
+    def __init__(self, process_name, process_pid=-1):
+        self.name = process_name
+        self.pid = process_pid
+
+    def __get_path(self, file_name):
+        """
+        :param file_name: name of file with type
+        """
+        return os.getcwd() + os.sep + 'resources' + os.sep + file_name
+
+    def __get_date(self):
+        return datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+
+    def csv_write(self, data):
+        file_name = self.name.rsplit(".", 1)[0] + ".csv"
+        file_path = self.__get_path(file_name)
+        with open(file_path, mode="w", newline="") as file:
+            header = ["Data", "Process_name", "Process_pid", "Cpu_percent", "Cpu_times_user",
+                      "Cpu_times_system", "Memory_percent", "Memory_rss", "Memory_vms"]
+            write_file = csv.writer(file, delimiter=";")
+            write_file.writerow(header)
+            write_file.writerows(data)
+
+    def get_process_stat(self, min_steps=5, timer_subprocess=1):
+
+        """
+        :param min_steps: how much scan for process
+        :param timer_subprocess: how much seconds for 1 subprocess
+        :return: list of data
+        """
+
+        result = []
+        for times in range(min_steps):
+            for process in psutil.process_iter():
+                check_condition = process.name() == self.name if self.pid == -1 else process.pid == self.pid
+                with process.oneshot():
+                    if check_condition:
+                        for i in range(timer_subprocess):
+                            temp = [
+                                self.__get_date(),
+                                process.name(),
+                                process.pid,
+                                process.cpu_percent(),
+                                process.cpu_times().user,
+                                process.cpu_times().system,
+                                process.memory_percent(),
+                                process.memory_info().rss / (1024 * 1024),
+                                process.memory_info().vms / (1024 * 1024)
+                            ]
+                        temp = list(
+                            map(lambda x: " " + str(round(x, 2)) + " " if type(x) == float else str(x) + " ", temp))
+                        result.append(temp)
+                        time.sleep(1.0)
+            time.sleep(1.0)
+        return result
 
 
-def process_statistics(process_name):
-    result = []
-    for process in psutil.process_iter():
-        with process.oneshot():
-            if process.name() == process_name:
-                # result.append({Date:})
-                print(process.name(), process.memory_info(), sep="  \n")
-                break
-    # for process in psutil.process_iter():
-    # if process.name() == "chrome.exe":
-    #     print(process)
-
-# process_statistics(NAME)
+'''
+if __name__ == '__main__':
+    monitor = Monitor(sys.argv[1])
+    monitor.csv_write(monitor.get_process_stat())
+'''
--- selenium_tests.py
+++ selenium_tests.py
@@ -1,11 +1,34 @@
+import time
+
 import selenium.webdriver as webdriver
 
+import processes as pc
 
-SEARCH_BOX_LOCATOR = "[class^='theme__inputElement']"
+# SEARCH_BOX_LOCATOR = "[class^='theme__inputElement']"
+GOOGLE_URL = "https://www.google.com/"
 
-driver = webdriver.Chrome("resources/chromedriver.exe")
-driver.get("https://weather.com/")
-search_box_element = driver.find_element_by_css_selector(SEARCH_BOX_LOCATOR)
+monitor = pc.Monitor("chromedriver.exe")
 
-print("wait")
-# driver.close()
+
+def go_to(text):
+    time_go = 2
+
+    driver = webdriver.Chrome("resources/chromedriver.exe")
+    monitor.csv_write(monitor.get_process_stat())
+    driver.get(GOOGLE_URL)
+    monitor.csv_write(monitor.get_process_stat())
+    element = driver.find_element_by_name("q")
+    monitor.csv_write(monitor.get_process_stat())
+    element.send_keys(text)
+    monitor.csv_write(monitor.get_process_stat())
+    element.submit()
+
+    for num in range(time_go):
+        list_elements = driver.find_elements_by_css_selector("div.r > a:first-child")
+        list_elements[num].click()
+        driver.execute_script("window.history.go(-1)")
+        time.sleep(1)
+    # search_box_element = driver.find_element_by_css_selector(SEARCH_BOX_LOCATOR)
+
+
+go_to("Joker")
