945284efd79c57db94cefa1d8b6410253a2241ee
bohdan_kulchytskyy <merseneries@gmail.com>
2019-12-23 15:47:38 +0200

    Deleted test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-last = 0
-with open("number", "r") as file:
-    lines = file.readlines()
-    last = int(lines[-1])
-    print(last)
-    last += 1
-
-with open("number", "a") as file:
-    file.write("\n" + str(last))
