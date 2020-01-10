80e829af6c53e9d668a22e0cd59b1bacd61cf43b
bohdan_kulchytskyy <merseneries@gmail.com>
2020-01-10 18:17:13 +0200

    Updated and add test cases

--- voice/test_cortana.py
+++ voice/test_cortana.py
@@ -2,12 +2,15 @@ import time
 import unittest
 
 import pyautogui as autogui
+import pytesseract
 import pyttsx3
+import wolframalpha
 from appium import webdriver
 
 BROWSER = "Open microsoft.com"
 PATH_CORTANA = r"C:\Windows\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy\SearchUI.exe"
 PATH_WINDRIVER = r"C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe"
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 
 class Test_Cortana(unittest.TestCase):
@@ -25,14 +28,15 @@ class Test_Cortana(unittest.TestCase):
 
     def setUp(self):
         self.engine = pyttsx3.init()
-        self.engine.setProperty('rate', 120)
+        self.engine.setProperty('rate', 110)
         self.open_cortana()
 
-    def tearDown(self):
-        autogui.press("esc")
+    # def tearDown(self):
+    #     autogui.press("esc")
 
     def open_cortana(self):
         self.engine.say("Hey Cortana")
+        # autogui.hotkey("win", "c")
 
     def say(self, text, timer=2):
         self.engine.say(text)
@@ -51,39 +55,96 @@ class Test_Cortana(unittest.TestCase):
                 time.sleep(1)
                 count += 1
 
-    def test_text(self):
-        text_question = "Who was the first president of USA?"
+    @staticmethod
+    def get_screenshot_text(area=(0, 0, 0, 0)):
+        scr = autogui.screenshot(region=area)
+        scr = scr.convert("L")
+        first_pixel = scr.getpixel((0, 0))
+        if first_pixel != 0 and first_pixel != 255:
+            scr = scr.point(lambda x: 0 if x > 200 else 255)
+        scr.save("image.jpeg")
+        return pytesseract.image_to_string(scr)
 
+    @staticmethod
+    def get_woflramalpha_result(request_text):
+        client = wolframalpha.Client("TPLGT3-ETLWX6G6X3")
+        res = client.query(request_text)
+        return next(res.results).text
+
+    @staticmethod
+    def check_response(actual, expected):
+        actual = actual.lower().replace(".", "").replace(",", "").split()
+        expected = expected.lower().replace(".", "").replace(",", "").split()
+        return any(i for i in actual if i in expected)
+
+    def test_text(self):
+        text_question = "Who was the first President of USA?"
         self.say(text_question)
+
+        # check if response about President
         cortana_response = self.driver.find_element_by_class_name("TextBlock").text
-        self.assertIn("president", cortana_response, "Search not about president")
+        self.assertIn("president", cortana_response, "Search not about President")
+
+        # check if result correct President
+        cortana_result = self.get_screenshot_text(area=(45, 735, 340, 55))
+        woflram_result = self.get_woflramalpha_result(text_question)
+        self.assertIn(cortana_result, woflram_result, "Result not correct")
 
     def test_weather(self):
         text_city = "weather in Vinnytsya"
-
         self.say(text_city)
-        # cortana_response = self.driver.find_element_by_name("TextBlock").text
+
+        # check if response about weather
+        cortana_response = self.get_screenshot_text(area=(50, 471, 340, 30))
+        self.assertTrue(self.check_response(text_city, cortana_response), "Search not about weather")
+
+        # check if correct city selected
+        cortana_result = self.get_screenshot_text(area=(50, 525, 160, 31))
+        self.assertTrue(self.check_response(text_city, cortana_result), "Incorrect city")
 
     def test_calculator(self):
-        text_math = "2 asterisk 18 equal"
+        text_math = "2 multiply by 18 equal"
         math_actual = 2 * 18
-
         self.say(text_math)
+
+        # check if response about calculation
         cortana_response = self.driver.find_element_by_class_name("WebView").text
-        self.assertIn(text_math, cortana_response, "Text '{0}' not in Cortana response".format(text_math))
+        # self.assertIn(text_math, cortana_response, "Text '{0}' not in Cortana response".format(text_math))
+        self.assertTrue(self.check_response(text_math, cortana_response), "Search not about calculator")
 
-        cortana_answer = self.driver.find_element_by_accessibility_id("rcHead").text
-        self.assertEqual(str(math_actual), cortana_answer, "{0} don't equal {1}".format(math_actual, cortana_answer))
+        # check if correct calculation
+        cortana_result = self.driver.find_element_by_accessibility_id("rcHead").text
+        self.assertEqual(str(math_actual), cortana_result, "{0} don't equal {1}".format(math_actual, cortana_result))
 
-    def test_open(self):
-        text_open = "Calendar"
-
+    def test_open_calendar(self):
+        text_open = "calendar"
         self.say(text_open)
-        cortana_response = self.driver.find_element_by_class_name("WebView").text
-        self.assertIn(text_open, cortana_response,
-                      "Trying to open '{0}' but response is '{1}'".format(text_open, cortana_response))
+
+        # check if response about calendar
+        # cortana_response = self.driver.find_element_by_class_name("WebView").text
+        cortana_response = self.get_screenshot_text(area=(50, 500, 325, 60))
+        self.assertTrue(self.check_response(text_open, cortana_response), "Search not about calendar")
+        # self.assertIn(text_open, cortana_response,"Trying to open '{0}' but response is '{1}'".format(text_open, cortana_response))
         self.wait_listen()
         self.say(text_open)
+
+        # check if correct program opened
+        cortana_result = self.get_screenshot_text(area=(60, 505, 325, 40))
+        self.assertTrue(self.check_response(text_open, cortana_result), "Must opened calendar")
+
+    def test_convert(self):
+        text_convert = "How much seconds in week"
+        actual_convert = 7 * 24 * 60 * 60
+        self.say(text_convert)
+
+        # check if response about convert
+        cortana_response = self.get_screenshot_text(area=(55, 532, 100, 25))
+        self.assertTrue(self.check_response(text_convert, cortana_response), "Search not about convert")
+
+        # check if correct convert result
+        cortana_result = self.get_screenshot_text(area=(55, 561, 121, 30))
+        self.assertTrue(self.check_response(str(actual_convert), cortana_result))
+        print(cortana_result)
 
 
 unittest.main()
