c5f06d49090dea9e88461467861907fd596768bc
bohdan_kulchytskyi <merseneries@gmail.com>
2020-02-20 16:40:26 +0200

    Add decorator of time
    Add decorator for runtime check of the function

--- _some_staff/sort.py
+++ _some_staff/sort.py
@@ -1,3 +1,6 @@
+import time
+import random
+
 two_dimensional = [
     [12, 45, 1, 0, 66],
     [47, 32, 11, 7, 17],
@@ -5,15 +8,29 @@ two_dimensional = [
     [0, -16, 22, 12, 33]
 ]
 
+one_dim = [random.randint(-100, 100) for i in range(100)]
+
+
+def timer(func):
+    def wrapper(arg):
+        start = time.time_ns()
+        arr = func(arg)
+        end = time.time_ns()
+        print(f"Run time: {end - start} ns")
+        return arr
+
+    return wrapper
+
 
 def list_no_duplicates(array):
     one_dimensional = []
     for i in array:
         one_dimensional += i
-    return sort_list(one_dimensional), len(one_dimensional)
+    return bubble_sort(one_dimensional), len(one_dimensional)
 
 
-def sort_list(array):
+@timer
+def bubble_sort(array):
     arr_len = len(array)
 
     if arr_len == 0:
@@ -22,10 +39,15 @@ def sort_list(array):
     for j in range(arr_len):
         for i in range(arr_len - 1):
             if array[i] > array[i + 1]:
-                tmp = array[i]
-                array[i] = array[i + 1]
-                array[i + 1] = tmp
+                array[i], array[i + 1] = array[i + 1], array[i]
     return array
+
+
+def count_sort(array):
+    """
+        One loop that count frequency of numbers. Only for +int?
+    """
+    pass
 
 
 result_no_ordered, list_length = list_no_duplicates(two_dimensional)
